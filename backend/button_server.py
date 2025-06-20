86c68eb3e895a387b668a9387e6777570027712a
Watson233 <115355502+Watson277@users.noreply.github.com>
2025-06-20 10:00:03 +0200

    Add files via upload

--- backend/button_server.py
+++ backend/button_server.py
@@ -1,10 +1,10 @@
 # gpio_router.py
 from fastapi import APIRouter
 import lgpio
-from variables import button_state
+from variables.button_state import button_val
 
 router = APIRouter()
-
+val = 0
 # GPIO setup
 PIN = 26
 h = lgpio.gpiochip_open(0)
@@ -12,6 +12,9 @@ lgpio.gpio_claim_input(h, PIN, lgpio.SET_PULL_UP)
 
 @router.get("/button")
 def read_button():
+
     """Returns the current state of the button."""
-    button_state = lgpio.gpio_read(h, PIN)
-    return {"state": button_state}
+    button_val = lgpio.gpio_read(h, PIN)
+    val = lgpio.gpio_read(h, PIN)
+    #print("button_val",button_val)
+    return {"state": button_val}
--- backend/button_speak.py
+++ backend/button_speak.py
@@ -1,15 +1,32 @@
 # button_reactor.py
 import time
 from api.voice_server import run_speech_once
-from variables import button_state
+import lgpio
+import requests
+import asyncio
+
+
 print("run button service success")
 
+
 def monitor_button_state():
-    print("detect button state")
-    print("val",button_state)
-    if button_state == 1:  # Rising edge
-        print("🔁 [button_reactor] Button state triggered!")
-        run_speech_once()
-    time.sleep(0.1)
+    print("🔁 Monitoring button... (will exit on press)")
+    while True:
+        try:
+            response = requests.get("http://localhost:8000/button")
+            if response.status_code == 200:
+                data = response.json()
+                print("🟢 Button state:", data["state"])
+                if data["state"] == 1:
+                    run_speech_once()
+                    print("✅ Button pressed! Exiting loop.")
+                    
+
+            else:
+                print("❌ Failed to get button state")
+        except Exception as e:
+            print("❌ Error:", e)
+
+        time.sleep(0.2)  # check every 200 ms
 
 
